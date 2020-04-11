808866eaf0bbab680d75e895090d49a201a5efe8
Andre Faria <andremarcalfaria@gmail.com>
2020-04-11 13:27:52 +0100

    minor update

--- cowsay-cowthink-fortune-steroids.sh
+++ cowsay-cowthink-fortune-steroids.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Put this on your ".*rc" after installing cowsay and fortune
-# Note: When using any shell different from bash you need to have bash installed
+#Put this on your ".*rc" after installing cowsay and fortune
 
 COMMANDS=(cowsay cowthink)
 COW=$(ls /usr/share/cowsay/cows/ | shuf -n 1)
@@ -9,5 +8,5 @@ EYES=$(cat /dev/urandom | tr -dc 'A-Z0-9' | fold -w 1 | head -n 1)
 TONG=$(cat /dev/urandom | tr -dc 'A-Z0-9' | fold -w 1 | head -n 1)
 
 if [  -x $(which cowsay) -a -x $(which cowthink) -a -x $(which fortune) ]; then
-  /bin/bash -c "fortune -a | $(echo ${COMMANDS[RANDOM % ${#COMMANDS[@]}]}) -f $COW -e $EYES$EYES -T $TONG$TONG"
+  fortune -a | $(echo ${COMMANDS[RANDOM % ${#COMMANDS[@]}]}) -f $COW -e $EYES$EYES -T $TONG$TONG
 fi
