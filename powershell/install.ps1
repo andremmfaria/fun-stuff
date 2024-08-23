b50c694fa72d1fdd0d25231ee6e5c843b258ab31
Andre Faria <andremarcalfaria@gmail.com>
2024-08-23 10:37:52 +0100

    Update install.ps1

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -6,7 +6,6 @@ Install-Module posh-git -Scope CurrentUser -Force
 
 echo "Installing oh-my-posh... (accept any prompt message that appears)"
 winget install --id JanDeDobbeleer.OhMyPosh -s winget
-$env:Path += ";$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\bin"
 
 echo "Installing winfetch... (accept any prompt message that appears)"
 Install-Script winfetch -Scope CurrentUser -Force
@@ -15,4 +14,3 @@ mv .\.config $env:USERPROFILE
 echo "Installing fzf... (accept any prompt message that appears)"
 winget install --id junegunn.fzf -s winget
 Install-Module -Name PSFzf -Scope CurrentUser -Force
-
